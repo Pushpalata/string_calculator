4b90a9f359e5d597a1938e05faa91e9092bb9597
Pushpalata Chauhan <>
2025-03-22 21:15:43 +0530

    comma checking if forward slash not present

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,8 +2,6 @@ class StringCalculator
   def add(str)
     return 0 if str.to_s.strip.empty?
 
-    return str.to_i unless str.include?(',')
-
     if str.start_with?("//")
       str = str.gsub("//", "")
       str_parts = str.split("\n")
@@ -11,6 +9,7 @@ class StringCalculator
       str = str_parts.last
       str.split("#{delimiter}").map(&:to_i).sum
     else
+      return str.to_i unless str.include?(',')
       str.split(/[\n,]/).map(&:to_i).sum
     end
   end
