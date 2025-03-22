5a1879dc1e69e33d6ba00251639887b072146806
Pushpalata Chauhan <>
2025-03-22 21:20:37 +0530

    Refactor | split by delimiter should be common

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,15 +2,13 @@ class StringCalculator
   def add(str)
     return 0 if str.to_s.strip.empty?
 
+    delimiter = /[\n,]/
     if str.start_with?("//")
       str = str.gsub("//", "")
       str_parts = str.split("\n")
       delimiter = str_parts.first
       str = str_parts.last
-      str.split("#{delimiter}").map(&:to_i).sum
-    else
-      return str.to_i unless str.include?(',')
-      str.split(/[\n,]/).map(&:to_i).sum
     end
+    str.split(delimiter).map(&:to_i).sum
   end
 end
