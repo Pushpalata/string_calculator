26d48555305a786261b026c151feeb12c12cc6c5
Pushpalata Chauhan <>
2025-03-22 20:05:50 +0530

    Test for blank string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,18 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
 
+  describe "#add" do
+    context "blank string" do
+      it "returns 0 for blank string" do
+        expect(calculator.add("")).to eq(0)
+      end
+
+      it "returns 0 for nil" do
+        expect(calculator.add(nil)).to eq(0)
+      end
+
+    end
+  end
 end
