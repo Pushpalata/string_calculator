85178eaeb3437671ebd743c14a0b193292b4d044
Pushpalata Chauhan <>
2025-03-22 21:48:25 +0530

    Handle special character as delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,7 +5,7 @@ class StringCalculator
     delimiter = /[\n,]/
     if str.start_with?("//")
       delimiter_line, str = str.split("\n", 2)
-      delimiter = delimiter_line[2..]
+      delimiter = Regexp.escape(delimiter_line[2..])
     end
     numbers = str.split(/#{delimiter}/).map(&:to_i)
 
