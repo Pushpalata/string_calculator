134a4966d8da95a9073fc60f8635ee27d8c14e62
Pushpalata Chauhan <>
2025-03-22 21:29:02 +0530

    Use String slicing instead of split

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,7 +5,7 @@ class StringCalculator
     delimiter = /[\n,]/
     if str.start_with?("//")
       delimiter_line, str = str.split("\n", 2)
-      delimiter = delimiter_line.split("//").last
+      delimiter = delimiter_line[2..]
     end
     str.split(delimiter).map(&:to_i).sum
   end
