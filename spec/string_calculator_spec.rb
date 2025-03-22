75793ee84eb827a87b6bfff3d604b78a0bb11a64
Pushpalata Chauhan <>
2025-03-22 20:23:09 +0530

    Give sum when number comma separated

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -16,7 +16,13 @@ RSpec.describe StringCalculator do
       it "returns 0 for string with spaces" do
         expect(calculator.add("  ")).to eq(0)
       end
-
     end
+
+    context "valid numbers" do
+      it "returns sum when number passed comma separated" do
+        expect(calculator.add("1,2")).to eq(3)
+      end
+    end
+
   end
 end
